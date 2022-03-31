057b4decc276cfb50e68c13503e19e99b39adc43
Serahill <ville@daini.dev>
2022-04-01 01:02:56 +0300

    2.0
    Made the code cleaner in general and added comments

--- run.js
+++ run.js
@@ -1,35 +1,53 @@
 console.time("script");
+
+
+// I decided to use arrays to represent both devices and networks
+// For devices the syntax is [z, y].
+// Networks use the same syntax but the 3rd value in networks is for reach [x, y, reach].
+
 const networks = [[0, 0, 9], [20, 20, 6], [10, 0, 12], [5, 5, 13], [99, 25, 2]];
 const devices = [[0, 0], [100, 100], [15, 10], [18, 18], [13, 13], [25, 99]];
 let results = [];
 
-console.time("calculations");
 
-for (let i = 0; i < devices.length; i++) {
-	let res = [];
-	res[0] = 0;
-	for (let ii = 0; ii < networks.length; ii++) {
-		let x = Math.abs(devices[i][0] - networks[ii][0]);
-		let y = Math.abs(devices[i][1] - networks[ii][1]);
-		let d = Math.sqrt(x**2+y**2);
-		if (d <= networks[ii][2]) {
-			let s = (networks[ii][2] - d)**2;
-			if (res[0] < s) {
-				res[0] = s;
-				res[1] = ii;
-			}
+
+// I ended up using forEach due to it being cleaner and easier to follow
+// as opposed to alternatives like 'for'. I would use 'for' if we had a larger dataset,
+// but at this scale the slower 'forEach' makes no difference
+
+// [0] = x
+// [1] = y
+// [2] = reach
+
+devices.forEach( device => {
+	let res = [0];
+	networks.forEach ( (network, index) => {
+		// Calculating distance
+		let x = Math.abs(device[0] - network[0]);
+		let y = Math.abs(device[1] - network[1]);
+		let d = Math.sqrt(x ** 2 + y ** 2);
+		// Ignore networks which are out of range
+		if (d <= network[2]) {
+			// Calculating connection speed
+			let s = (network[2] - d) ** 2;
+			if (res[0] < s) res = [s, index];
 		}
-	}
+	})
 	results.push(res);
-}
+})
 
-console.timeEnd("calculations");
+results.forEach( (result, i) => {
 
-for (let p = 0; p < results.length; p++) {
-	if (results[p][0] === 0) {
-		console.log(`No network station within reach for point ${devices[p][0]},${devices[p][1]}`);
-	} else {
-		console.log(`Best network station for point ${devices[p][0]},${devices[p][1]} is ${networks[results[p][1]][0]},${networks[results[p][1]][1]} with speed ${results[p][0]}`);
-	}
-}
+	// I decided to declare a 'shorthand' variables here to avoid using too long variables
+	// such as networks[results[1]][0] in middle of a message builder
+	let device = devices[i];
+	let network = networks[result[1]];
+
+	let msg = result[0] === 0
+		? `No network station within reach for point ${device[0]},${device[1]}`
+		: `Best network station for point ${device[0]},${device[1]} is ${network[0]},${network[1]} with speed ${result[0]}`
+	console.log(msg);
+
+})
+
 console.timeEnd("script");
